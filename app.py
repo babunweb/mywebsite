26d69fcbafe48c7ff92a030de8bd5e92c03f7728
babunweb <babnithaj786@gmail.com>
2026-04-21 20:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello! Idhu en first website 🔥"
 
-# IMPORTANT FOR RAILWAY
-port = int(os.environ.get("PORT", 8080))
-
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
