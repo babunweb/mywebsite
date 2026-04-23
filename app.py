daa14890a59fa4c44abcf78844863f4adcecbfad
babunweb <babnithaj786@gmail.com>
2026-04-23 08:32:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Idhu en first website 🔥"
+    return """
+    <h1 style='color:blue;'>🔥 Welcome to my website</h1>
+    <p>This is my first website 🚀</p>
+
+    <button onclick="alert('Hello da 😎')">Click Me</button>
+    """
